45d24e88af81c05038b198c5d76147482ff8edb2
Suryansh <suryanshdev09@gmail.com>
2023-07-30 20:09:16 +0530

    commiting changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import React, { Component } from "react";
-import Navbar from "./Components/NavBar";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar></Navbar>
+        <Navbar />
+        <News></News>
       </div>
     );
   }
