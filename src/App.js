5a34aeae7e94f7e56b8c9c3dd98974d4170ba726
Suryansh <suryanshdev09@gmail.com>
2023-08-02 21:32:51 +0530

    Changing Page Size

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
     return (
       <div>
         <Navbar />
-        <News pageSize={5} country={"in"} />
+        <News pageSize={6} country={"in"} />
       </div>
     );
   }
